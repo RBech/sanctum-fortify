6123248b7c6fd415a9f86e96e0ad8b8c492ba753
Rasmus Bech <me@rbech.com>
2021-10-20 20:34:09 +0200

    Add seeder for default user

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
